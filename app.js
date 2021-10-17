3f1ad0ae1813a806618eddbe5ed6aad49b3479a4
Dahiam Castano <dahiamps@gmail.com>
2021-10-17 17:16:10 -0500

    Se crea el metodo para crear usuario desde el api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 
 const app = express();
-
+app.use(express.json());
 
 //1 - Invocamos a la conexion de la DB
 const connection = require('./database/db');
@@ -17,14 +17,26 @@ app.get('/', function (req, res) {
 
 app.get('/api/usuarios', (req, res) => {
     connection.query('SELECT * FROM usuarios', (error, respuesta) => {
-        if(error){
+        if (error) {
             throw error;
         } else {
             res.send(respuesta)
         }
     })
 })
-// levantamos servidor 
-app.listen('3000', function () {
-    console.log(`servidor OK`);
-})
+
+app.post('/api/usuarios', (req, res) => {
+    let data = { Nombre: req.body.Nombre, Usuario: req.body.Usuario, Contraseña: req.body.Contraseña, Pais: req.body.Pais, Ciudad: req.body.Ciudad, Direccion: req.body.Direccion };
+    let sql = "INSERT INTO usuarios SET ?";
+    connection.query(sql, data, function (error, results) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(results)
+        }
+    });
+});
+    // levantamos servidor 
+    app.listen('3000', function () {
+        console.log(`servidor OK`);
+    })
